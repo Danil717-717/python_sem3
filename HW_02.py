0abda5977b313a0a84509699a3f2a3b21d09a42a
Danil <danila_monster@mail,ru>
2022-10-04 15:32:18 +0300

    HW_002

--- HW_02.py
+++ HW_02.py
@@ -4,14 +4,28 @@
 #  in 4  out  [2, 5, 8, 10]    [20, 40]
 #  in 5  out  [2, 2, 4, 8, 8]  [16, 16, 4]
 
-import random
+from random import sample
 
-num = int(input())
-num_list = random.sample(range(0, 40), num)
-print(num_list)
+def list_nums(count: int):
+    if count < 0:
+        print("Ввели отрцатльное число")
+        return []
 
-def product_pairs(num_list):
-    if num_list % 2 == 0:
-        for i in range(num_list):
-           num_list[i] * num_list[i - 1]
+    list_nums = sample(range(1, count * 2), count)
+    return list_nums 
+
+def product_pairs(list_nums: list):
+    new_list = []
+    len_list = len(list_nums)
+
+    for i in range(len_list // 2):
+        new_list.append(list_nums[i] * list_nums[len_list - i - 1])
+
+    if len_list % 2:
+        new_list.append(list_nums[len_list // 2])
+    return new_list
+
+all_list = list_nums(int(input("Введите количество чисел: ")))
+print(all_list)
+print(product_pairs(all_list))            
             
